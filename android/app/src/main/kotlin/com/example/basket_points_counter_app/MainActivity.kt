955b6ecc519c6b25cc9c349ea36c28db016a2768
Eslam Fares Mohamed Emam <152084165+bgbos7077@users.noreply.github.com>
2024-09-19 15:58:38 +0300

    nitial commit

---
+++ android/app/src/main/kotlin/com/example/basket_points_counter_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.basket_points_counter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PointsCounter());
+}
+
+class PointsCounter extends StatelessWidget {
+  const PointsCounter({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
